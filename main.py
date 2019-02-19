f9ff73b221683eb42b68b0cf4a2f6a3eef5bc81e
adonayab <adonayab15@hotmail.com>
2019-02-18 18:54:30 -0600

    Added html titles to each template, Added list of users to the index page

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def login():
         elif user and user.pw_hash != password:
             flash('Incorrect password')
         elif email == '' and password == '':
-            flash('Email and Password can not be empty')
+            flash('Email and Password required')
         else:
             flash('User does not exist')
 
-    return render_template('login.html')
+    return render_template('login.html', title='Blogz Login')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -52,24 +52,24 @@ def signup():
 
         if email == '' and password == '' and verify == '':
             flash('Email and Password fields can not be empty')
-            return render_template('signup.html')
+            return render_template('signup.html', title='Blogz Signup')
 
         if existing_user:
             flash('{} Already an account'.format(email))
-            return render_template('signup.html')
+            return render_template('signup.html', title='Blogz Signup')
         if email == '':
             flash('Email field required')
-            return render_template('signup.html')
+            return render_template('signup.html', title='Blogz Signup')
         if not existing_user and password == '':
             flash('Password field required')
-            return render_template('signup.html')
+            return render_template('signup.html', title='Blogz Signup')
         if not existing_user and (len(password) < 3 or len(password) > 20) and password != verify:
             flash('Invalid Password')
             flash('Passwords do not match')
-            return render_template('signup.html')
+            return render_template('signup.html', title='Blogz Signup')
         if not existing_user and password != verify:
             flash('Passwords do not match')
-            return render_template('signup.html')
+            return render_template('signup.html', title='Blogz Signup')
 
         user = User(email=email, password=password)
         db.session.add(user)
@@ -78,7 +78,7 @@ def signup():
 
         return redirect('/')
 
-    return render_template('signup.html')
+    return render_template('signup.html', title='Blogz Signup')
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -103,29 +103,31 @@ def newpost():
             flash("A blog with the same title exists. Please choose a different title.")
             return redirect('/newpost')
 
-    return render_template('newpost.html')
+    return render_template('newpost.html', title='Blogz Post')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    blogs = Blog.query.all()
-    return render_template('index.html', blogs=blogs)
+    users = User.query.all()
+    return render_template('index.html', users=users, title='Blogz Home')
 
 
 @app.route('/blog', methods=['POST', 'GET'])
-def blog():
-    blogs = Blog.query.all()    
+def blog():    
     blog_id = request.args.get('id')
     user_id = request.args.get('user')
+
+    blogs = Blog.query.all()
     blog = Blog.query.filter_by(id=blog_id).first()
     user_blogs = Blog.query.filter_by(owner_id=user_id).all()
     user_name = Blog.query.filter_by(owner_id=user_id).first()
+
     if blog_id:
         return render_template('blog.html', title="Blog", blog_title=blog.title, body=blog.body)
     if user_blogs:
         return render_template('blog.html', title="Blog", blog_title=user_name.owner.email, user_blogs=user_blogs)
     else:
-        return render_template('blog.html', title="Blog", blogs=blogs, blog_title='Build a Blog')
+        return render_template('blog.html', title="Blogz", blogs=blogs, blog_title='Build a Blog')
 
 
 if __name__ == "__main__":
